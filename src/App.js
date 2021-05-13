c961f2667beb60b2a1041e8c5c843ef4258274b5
ac066849 <adil.c@cerner.com>
2021-05-13 13:27:55 +0530

    on window resize re-render fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react'
 import './App.css';
-import MainContainer from './conatiner-components/main-container'
+import MainContainer from './conatiner-components/main-container';
+
+function debounce(fn, ms) {
+  let timer
+  return _ => {
+    clearTimeout(timer)
+    timer = setTimeout(_ => {
+      timer = null
+      fn.apply(this, arguments)
+    }, ms)
+  };
+}
 
 function App() {
+  const [dimensions, setDimensions] = React.useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  })
+  React.useEffect(() => {
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      })
+    }, 500)
+
+    window.addEventListener('resize', debouncedHandleResize)
+
+    return _ => {
+      window.removeEventListener('resize', debouncedHandleResize)
+
+    }
+  })
+  console.log("dimensions= ",dimensions)
   return (
     <div className="App">
       <MainContainer />
